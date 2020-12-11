d473f306f69a3e0dd975a0a55620de5a3da22ee9
Anita-Naragund <72910010+Anita-Naragund@users.noreply.github.com>
2020-12-11 10:59:29 +0530

    My commit

---
+++ src/com/xworkz/patterns/Pattern.java
@@ -0,0 +1,19 @@
+package com.xworkz.patterns;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=4;
+		for(int i=0;i<=num;i++)
+		{
+			for(int j=0;j<=num;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
