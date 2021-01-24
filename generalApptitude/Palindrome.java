c0ff374bc8c127c49c7ea5599a03dde05173b953
Anita-Naragund <72910010+Anita-Naragund@users.noreply.github.com>
2021-01-24 18:50:49 +0530

    ArmStrong,Anagram,Fibonacci,Palindrome

---
+++ generalApptitude/Palindrome.java
@@ -0,0 +1,22 @@
+package com.xworkz.patterns.generalApptitude;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		int num = 24;
+		int reverse = 0; 
+		int remainder, number;
+
+		number = num;
+		while (num != 0) {
+			remainder = num % 10;
+			reverse = reverse * 10 + remainder;
+			num /= 10;
+		}
+
+		if (number == reverse)
+			System.out.println(number + " is a palindrome.");
+		else
+			System.out.println(number + " is not a palindrome.");
+	}
+}
